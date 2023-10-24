af623184f1a746f194173b111b5b50ca12e3620c
Emesson Silva <140443316+emessonSilva@users.noreply.github.com>
2023-10-24 01:19:10 -0300

    Add files via upload

---
+++ atv1_typescript.ts
@@ -0,0 +1,38 @@
+// 1. Implemente o código do slide de número 6.
+console.log("Estudar é muito bom.");
+console.log("Paciência e persistência.");
+console.log("Revisão é a mãe do aprendizado.");
+
+// 2. Implemente o código do slide de número 15.
+
+// 3. Implemente o código do slide de número 24.
+
+// 4. Implemente o código do slide de número 33.
+
+
+// 5. Escreva uma função chamada "soma" que receba dois parâmetros (a e b) e retorne a soma deles.
+
+// 6. Crie uma função chamada "isPar" que receba um número inteiro como parâmetro e retorne "true"
+// se o número for par ou "false" caso contrário.
+
+
+// 7. Elabore uma função chamada "media" que receba três notas como parâmetros e retorne a média
+// aritmética delas.
+// 8. Elabore uma função chamada "imc" que receba o peso (em kg) e a altura (em metros) de uma
+// pessoa e retorne o índice de massa corporal (IMC) dela.
+// 9. Elabore uma função chamada "calcularDesconto" que receba o valor de um produto e o
+// percentual de desconto como parâmetros e retorne o valor com o desconto aplicado.
+// 10. Elabore uma função chamada "calcularImpostoRenda" que receba o salário bruto de uma pessoa
+// como parâmetro e retorne o valor do imposto de renda a ser pago, considerando as seguintes
+// faixas de renda:
+// a. Até R$ 1.903,98: isento
+// b. De R$ 1.903,99 até R$ 2.826,65: alíquota de 7,5%
+// c. De R$ 2 .826,66 até R$ 3.751,05: alíquota de 15%
+// d. De R$ 3.751,06 até R$ 4.664,68: alíquota de 22,5%
+// e. Acima de R$ 4.664,68: alíquota de 27,5%
+// 11. Faça uma função chamada "calcularMediaArredondada" que receba uma lista de números como
+// parâmetro e retorne a média aritmética desses números, arredondada para o inteiro mais
+// próximo.
+// 12. Desenvolva uma função chamada "contarDigitosParesImpares" que receba um número inteiro
+// como parâmetro e retorne a quantidade de dígitos pares e a quantidade de dígitos ímpares
+// presentes nesse número.
